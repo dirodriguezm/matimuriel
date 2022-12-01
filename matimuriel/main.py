55fc7582a567006f96afd045b7983617e5eb354f
diego <diego@pop-os.localdomain>
2022-12-01 15:45:07 -0300

    fix: added delimiter argument

--- matimuriel/main.py
+++ matimuriel/main.py
@@ -6,16 +6,39 @@ app = typer.Typer()
 
 
 @app.command()
-def main(mails_path: str, files_path: str, out_path: str, mail_column="mails"):
-    mails = pd.read_csv(mails_path)
+def main(
+    mails_path: str,
+    files_path: str,
+    out_path: str,
+    mail_column="mails",
+    delimiters=",XX;",
+):
+    delimiter_list = delimiters.split("XX")
+    mails = try_read_csv(Path(mails_path), delimiter_list)
     mails["repetido"] = False
-    files_dir = Path(files_path)
-    for path in files_dir.iterdir():
-        dataframe = pd.read_csv(path)
+    for path in Path(files_path).iterdir():
+        dataframe = try_read_csv(path, delimiter_list)
         mails["mask"] = mails[mail_column].isin(dataframe[mail_column])
         mails.repetido = mails.loc[:, ["repetido", "mask"]].any(axis=1)
     mails = mails.drop(columns="mask")
     mails.to_csv(out_path, index=False)
+
+
+def try_read_csv(path: Path, delimiters: list) -> pd.DataFrame:
+    dataframe = pd.DataFrame()
+    err = Exception("Error al leer csv")
+    is_err = False
+    for delim in delimiters:
+        try:
+            is_err = False
+            dataframe = pd.read_csv(path, delimiter=delim)
+        except Exception as exc:
+            is_err = True
+            err = exc
+        if not is_err:
+            return dataframe
+    if is_err:
+        raise err
 
 
 if __name__ == "__main__":
